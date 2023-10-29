58164c44e0fe33b911c8a41bb9327a286c0b44d0
Stephanie Rocha <orocha.stephanie@gmail.com>
2023-10-28 21:01:10 -0300

    Removing comments from app.py

--- app.py
+++ app.py
@@ -30,11 +30,3 @@ if __name__ == '__main__':
 
 #Importing views
 from system.adapters_entrypoints.api.routes import client_views, product_views, order_views
-
-#Product
-
-# #Order
-
-# #Payment
-
-# #Old Order (OrderProduct)
